7d6e05a292fd5fbfadc9c40a2b8392d1ee43d037
Gary Feltham <gary.feltham@citypay.com>
2018-09-05 14:10:03 +0100

    added executable bit

--- files/extract-pom-env.sh
+++ files/extract-pom-env.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # run in the same workdir directory as the pom.xml
-
 export PROJECT_VERSION=`xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml | sed -e 's/  *$//'`
 export PROJECT_NAME=`xmllint --xpath "//*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml | sed -e 's/  *$//' | sed -e 's/citypay-//'`
 export IMAGE_NAME=${PROJECT_NAME}:${PROJECT_VERSION}
+
 
